528be0aa20abb045f33be2ab10590dc9742105d8
Sinh Nguyen <sinh.nguyen@asnet.com.vn>
2025-01-07 14:26:14 +0700

    testing github variable

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,8 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Verify login feature', () => {
   test('Verify user login success!', async () => {
-    console.log('ENV', process.env.SAUCE_USERNAME);
-    console.log('ENV', process.env.SAUCE_ACCESS_KEY);
+    console.log('SAUCE_USERNAME', process.env.SAUCE_USERNAME);
+    console.log('SAUCE_ACCESS_KEY', process.env.SAUCE_ACCESS_KEY);
+    console.log('AGE', process.env.AGE);
+    console.log('NAME', process.env.NAME);
     expect(true).toBeTruthy();
   })
 });
