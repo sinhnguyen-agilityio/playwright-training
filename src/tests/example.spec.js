e5b8cb6666b38cce289a7b6161d9590c6f15f76f
Sinh Nguyen <sinh.nguyen@asnet.com.vn>
2025-01-08 14:43:30 +0700

    edit the test case

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,13 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Verify login feature', () => {
   test('Verify user login success!', async () => {
-    console.log('SAUCE_USERNAME', process.env.SAUCE_USERNAME);
-    console.log('SAUCE_ACCESS_KEY', process.env.SAUCE_ACCESS_KEY);
-    console.log('AGE', process.env.AGE);
-    console.log('NAME', process.env.NAME);
     expect(true).toBeTruthy();
-    expect(process.env.SAUCE_USERNAME).toEqual('SAUCE_USERNAME_VALUE');
-    expect(process.env.SAUCE_ACCESS_KEY).toEqual('SAUCE_ACCESS_KEY_VALUE');
     expect(process.env.AGE).toEqual('age1');
     expect(process.env.NAME).toEqual('value1');
   })
