d5675788449fa4d105ed4eee249bac8ded9ad2bf
Sinh Nguyen <sinh.nguyen@asnet.com.vn>
2025-01-08 15:40:11 +0700

    test secret key

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,7 +2,13 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Verify login feature', () => {
   test('Verify user login success!', async () => {
+    console.log('SAUCE_USERNAME', process.env.SAUCE_USERNAME);
+    console.log('SAUCE_ACCESS_KEY', process.env.SAUCE_ACCESS_KEY);
+    console.log('AGE', process.env.AGE);
+    console.log('NAME', process.env.NAME);
     expect(true).toBeTruthy();
+    expect(process.env.SAUCE_USERNAME).toBeTruthy();
+    expect(process.env.SAUCE_ACCESS_KEY).toBeTruthy();
     expect(process.env.AGE).toEqual('age1');
     expect(process.env.NAME).toEqual('value1');
   })
