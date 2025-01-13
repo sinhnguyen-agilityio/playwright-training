d457f3107078b55b0c123121ffc1093ae66fb38f
Sinh Nguyen <sinh.nguyen@asnet.com.vn>
2025-01-13 16:03:30 +0700

    check special character

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -2,14 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Verify login feature', () => {
   test('Verify user login success!', async () => {
-    console.log('SAUCE_USERNAME', process.env.SAUCE_USERNAME);
-    console.log('SAUCE_ACCESS_KEY', process.env.SAUCE_ACCESS_KEY);
     console.log('AGE', process.env.AGE);
-    console.log('NAME', process.env.NAME);
+    console.log('NAME_VALUE', process.env.NAME_VALUE);
     expect(true).toBeTruthy();
-    expect(process.env.SAUCE_USERNAME).toBeTruthy();
-    expect(process.env.SAUCE_ACCESS_KEY).toBeTruthy();
     expect(process.env.AGE).toEqual('age1');
-    expect(process.env.NAME).toEqual('value1');
+    expect(process.env.NAME_VALUE).toEqual('[9x[,+=]rF');
   })
 });
